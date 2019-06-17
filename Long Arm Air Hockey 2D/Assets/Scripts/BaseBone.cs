a0dbfb076396d64c0913df57c56958c2cafdf148
edem <devlet.seran@gmail.com>
2019-06-18 01:36:05 +0300

    minor fixes

---
+++ Long Arm Air Hockey 2D/Assets/Scripts/BaseBone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+	private Shoulder _Shoulder;
+	private PreShoulder _PreShoulder;
+
+	public Arm();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
