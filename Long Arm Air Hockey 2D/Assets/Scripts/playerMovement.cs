01a97a79ad799591fc379dbf923c52c410e5d38a
Albert <bosmervor1@gmail.com>
2019-06-18 01:14:37 +0300

    added solution

---
+++ Long Arm Air Hockey 2D/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed = 5.5f;
+    public float gravity = -9.8f;
+
+    private CharacterController _charController;
+
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(deltaX, deltaY, 0);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    }
+}
